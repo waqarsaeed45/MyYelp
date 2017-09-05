ce2131d06f823bbdfdcb199b02bb4038e93b8565
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-05 11:27:23 -0700

    add first passing test, had to configure working directory for JUnit to $

---
+++ app/src/test/java/compioneerx1/httpsgithub/myrestaurants/MainActivityTest.java
@@ -0,0 +1,33 @@
+package compioneerx1.httpsgithub.myrestaurants;
+
+import static junit.framework.Assert.assertTrue;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP, manifest = "app/manifests/AndroidManifest.xml")
+
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+}
