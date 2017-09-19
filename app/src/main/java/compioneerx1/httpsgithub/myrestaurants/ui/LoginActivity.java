7d7c27e7b50cc08aa2a895178a965a64c5e477d7
Alena Golovina and Sowmya Logaswamy <alena+sowmya@>
2017-09-19 10:00:53 -0700

    Add functionality for account registration

--- app/src/main/java/compioneerx1/httpsgithub/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/compioneerx1/httpsgithub/myrestaurants/ui/LoginActivity.java
@@ -1,15 +1,33 @@
 package compioneerx1.httpsgithub.myrestaurants.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import compioneerx1.httpsgithub.myrestaurants.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
