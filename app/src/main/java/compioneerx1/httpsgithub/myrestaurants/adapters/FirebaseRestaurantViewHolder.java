17baf33a3efa23873b38150076c531b9baf3462a
Alena Golovina and Sowmya Logaswamy <alena+sowmya@>
2017-09-25 12:09:54 -0700

    add XML animation

--- app/src/main/java/compioneerx1/httpsgithub/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/compioneerx1/httpsgithub/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -1,5 +1,7 @@
 package compioneerx1.httpsgithub.myrestaurants.adapters;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
@@ -57,20 +59,39 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
         ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
     }
 
+
+    //////////Use this for View Property Animation
+//    @Override
+//    public void onItemSelected() {
+//        itemView.animate()
+//                .alpha(0.7f)
+//                .scaleX(0.9f)
+//                .scaleY(0.9f)
+//                .setDuration(500);
+//    }
+//
+//    @Override
+//    public void onItemClear() {
+//        itemView.animate()
+//                .alpha(1f)
+//                .scaleX(1f)
+//                .scaleY(1f);
+//    }
+
+
     @Override
     public void onItemSelected() {
-        itemView.animate()
-                .alpha(0.7f)
-                .scaleX(0.9f)
-                .scaleY(0.9f)
-                .setDuration(500);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
     }
 
     @Override
     public void onItemClear() {
-        itemView.animate()
-                .alpha(1f)
-                .scaleX(1f)
-                .scaleY(1f);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext,
+                R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
     }
   }
